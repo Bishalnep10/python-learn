2ccb8c42c97a4afa47217e33826d0e27c17b9b54
Bishalnep10 <bishalnepali1010@gmail.com>
2025-07-10 18:32:29 +0545

    print numbers greater than 15 from a list of numbers

---
+++ basic/Q7.py
@@ -0,0 +1,8 @@
+"""
+7. Filtering with Loops:
+Given the list numbers = [5, 10, 15, 20, 25], write a for loop to print only the numbers that are greater than 15.
+"""
+numbers = [5, 10, 15, 20, 25]
+for number in numbers:
+    if number > 15:
+        print(number)
