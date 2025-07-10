68b2723e2789756721cd5641fbf86951dbbc1e8a
Bishalnep10 <bishalnepali1010@gmail.com>
2025-07-10 18:42:28 +0545

    print the index and value of colors

---
+++ basic/Q9.py
@@ -0,0 +1,10 @@
+"""
+9. Iterating with Enumerate:
+
+Use a for loop with the enumerate function to print both the index and value of each
+item in the list colors = ["red", "green", "blue"].
+"""
+colors = ["red", "green", "blue"]
+
+for index, color in enumerate(colors):
+    print(f"Index: {index}, Value: {color}")
