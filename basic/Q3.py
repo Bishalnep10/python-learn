20339a40368b24835ff9003cd351262dd4d1de86
Bishalnep10 <bishalnepali1010@gmail.com>
2025-07-10 18:06:24 +0545

    add numbers from 1-100

---
+++ basic/Q3.py
@@ -0,0 +1,10 @@
+"""
+3.Sum of Numbers:
+Write a for loop that calculates the sum of all numbers from 1 to 100.
+"""
+
+total_sum = 0
+for number in range(1, 101):
+    total_sum += number
+
+print(f"The sum of numbers from 1 to 100 is: {total_sum}")
