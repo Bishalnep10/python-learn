ac3353fae0f1c9a7436504d7f2c057ac419faabe
Bishalnep10 <bishalnepali1010@gmail.com>
2025-07-10 18:14:11 +0545

    print square of number from 1-5

---
+++ basic/Q4.py
@@ -0,0 +1,9 @@
+"""
+4.List Comprehension:
+Given the list numbers = [1, 2, 3, 4, 5], use a for loop to create a new list that contains the squares of each number.
+"""
+
+numbers = [1,2,3,4,5]
+for square in numbers:
+    print(square**2)
+
